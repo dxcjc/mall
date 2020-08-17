2ae6144bb597e62b923d44e98cfd4da175f83092
cjc <2462824378@qq.com>
2020-08-18 00:48:12 +0800

    实现轮播图和修改后台接口

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,37 +4,49 @@
   <nav-bar class="home-nav-bar">
    <h5 slot="center">购物街</h5>
  </nav-bar>
+ <home-swiper :banners="banners"/>
+
+
  </div>
+
 
 </template>
 
 <script>
- import {getHomeMultidata} from '@/network/home.js';
- import NavBar from '@/components/common/NavBar/NavBar'
+  import {getHomeMultidata} from '@/network/home.js';
+  import HomeSwiper from './childItem/HomeSwiper'
+  import NavBar from '@/components/common/NavBar/NavBar'
+ 
+  
+
+
   export default{
     name:"Home",
     components:{
-      NavBar
+      NavBar,
+      HomeSwiper
+      
     },
     data(){
       return{
-        result:null
+        banners:[],
+        recommend:[]
       }
     },
     created(){
       getHomeMultidata().then(res => {
-        this.result=res
-       
-      })
+            this.banners = res.data.banner.list;
+            this.recommend = res.data.recommend
+        })
     }
   }
 </script>
 <style scoped>
-#home {
-    padding-top: 44px;
-    height: 100vh;
-    position: relative;
-  }
+  #home {
+      padding-top: 44px;
+      height: 100vh;
+      position: relative;
+    }
 
   .home-nav-bar {
     background-color: var(--color-tint);
@@ -47,4 +59,6 @@
     z-index: 9;
 
   }
+
+
 </style>
